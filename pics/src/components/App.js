575814e31530fcf024af275bc8671ef9ab1b12e8
Nguyen Hong Phuc <stevephuc.dev@gmail.com>
2019-04-06 20:04:13 +0300

    lesson79

---
+++ pics/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SeachBar from './SearchBar'
+
+
+const App = ()=>{
+    return (
+        <div className='ui container' style={{marginTop:'50px'}}>
+            <SeachBar />
+
+        </div>
+    )
+}
+
+export default App
