a586938a9edbf5637aeed1af94fcbe7861d41f65
Nguyen Hong Phuc <stevephuc.dev@gmail.com>
2019-04-07 20:52:50 +0300

    lesson107

--- pics/src/components/imageCard.js
+++ pics/src/components/imageCard.js
@@ -2,13 +2,33 @@ import React from 'react';
 
 class ImageCard extends React.Component{
 
+    constructor(props){
+        super(props);
+        this.state={spans:0}
+        this.imageRef= React.createRef();
+    }
+
+    componentDidMount(){
+        this.imageRef.current.addEventListener('load',this.setSpans)
+        // console.log(this.imageRef);
+        // console.log(this.imageRef.current.clientHeight);
+        
+    }
+
+    setSpans=()=>{
+        const height=(this.imageRef.current.clientHeight);
+        const spans= Math.ceil(height / 10)
+        this.setState({spans})
+    }
+
     render(){
 
-        const {descripttion, urls}= this.props.image;
+        const {description, urls}= this.props.image;
         return(
-            <div>
+            <div style={{gridRowEnd:`span ${this.state.spans}`}}>
                 <img
-                    alt={descripttion}
+                    ref= {this.imageRef}
+                    alt={description}
                     src={urls.regular}
                 />
             </div>
